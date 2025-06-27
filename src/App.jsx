d4d903d06889b4ad019fd0eb6ecaffcc03aefe3b
daypunk <daehee3853@gmail.com>
2025-06-27 21:30:54 +0900

    - background color modified

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,12 @@ function App() {
 
   return (
     <div className="min-h-screen bg-slate-900 flex justify-center pt-20">
-      <div style={{ width: '300px', height: '300px' }}>
+      <div style={{ width: '220px', height: '220px' }}>
         <RiveComponent 
           style={{ 
             width: '100%', 
             height: '100%',
             imageRendering: 'auto',
-            opacity: 0.8
           }}
         />
       </div>
